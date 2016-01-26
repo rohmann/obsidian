aae725b1d71cb0b246712a681156d4c01eb1644b
Alexander Rohmann <alexander@rohmann.us>
2016-01-25 21:17:57 -0500

    add logging helpers

--- web/app/mu-plugins/helpers.php
+++ web/app/mu-plugins/helpers.php
@@ -4,12 +4,32 @@ function print_json( $data ) {
 	echo json_encode( $data, JSON_PRETTY_PRINT );
 }
 
-function dd( $data ) {
-	var_dump( $data );
-	die();
-}
+if ( ! function_exists('dd') ) :
+	function dd( $data ) {
+		var_dump( $data );
+		die();
+	}
+endif;
 
 function jsond( $data ) {
 	print_json( $data );
 	die();
+}
+
+function obsidian_log_clear() {
+	unlink(ABSPATH . "../obsidian.log");
+}
+
+function obsidian_log( $message ) {
+
+	ob_start();
+
+	if ( is_scalar( $message ) ) {
+		echo $message;
+	} else {
+		var_dump($message);
+	}
+	echo "\n\n";
+
+	error_log( ob_get_clean(), 3, ABSPATH . "../obsidian.log");
 }
