7763300660c4f5432123708143d5ad328f9a968a
Alexander Rohmann <alexander@rohmann.us>
2020-09-21 23:44:39 -0400

    log debug

--- config/environments/development.php
+++ config/environments/development.php
@@ -4,8 +4,13 @@ use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_LOG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
 
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
+
+ini_set('xdebug.var_display_max_depth', '10');
+ini_set('xdebug.var_display_max_children', '256');
+ini_set('xdebug.var_display_max_data', '1024');
