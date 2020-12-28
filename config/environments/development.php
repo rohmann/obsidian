3675155a6cf89fbf9f24a5fe8331f79d6e20791d
Alexander Rohmann <rohmann@Alexanders-MacBook-Pro.local>
2020-12-28 17:29:00 -0500

    allow file editing

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,6 +11,7 @@ Config::define('SCRIPT_DEBUG', true);
 // Enable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', false);
 
+define('FS_METHOD', 'direct');
 ini_set('xdebug.var_display_max_depth', '10');
 ini_set('xdebug.var_display_max_children', '256');
 ini_set('xdebug.var_display_max_data', '1024');
